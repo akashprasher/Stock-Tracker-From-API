b4713a750508997cd6e8ec3b5d52c351e7ce649d
akash prasher <prince.akash.prasher@gmail.com>
2021-03-09 13:47:26 +0530

    Updating the chart data with realtime
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-// Fetching Data From API
-// var apiUrl = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=GME&apikey=TB44BTAI1KG68RMV";
-// var apiUrl = "../api_local_data.json";
-
 const queryString = window.location.search; //?s={symbol}
 comSymbol = queryString.substring(3, queryString.length).toUpperCase();
+
 var apiUrl =
 	"https://www.alphavantage.co/query?function=OVERVIEW&symbol=" +
 	comSymbol +
 	"&apikey=TB44BTAI1KG68RMV";
+
 if (comSymbol.length < 1) {
 	comSymbol = undefined;
 }
@@ -18,14 +16,12 @@ if (comSymbol !== undefined) {
 	fetch(apiUrl)
 		.then((data) => data.json())
 		.then((data) => {
-			// console.log(data.Description);
 			document.getElementById("com-code").innerText = comSymbol;
 			document.getElementById("company-name").innerHTML = data.Name;
 
 			document.getElementById("company-description").innerHTML =
 				data.Description;
 			companyName = data.Name;
-			console.log(companyName);
 			ftCnt(companyName);
 
 			listDetailsHeading = [
@@ -41,7 +37,6 @@ if (comSymbol !== undefined) {
 				data.Sector,
 				data.Industry,
 			];
-			// valueDetails
 
 			var cardDataOne = `<div class="col-sm-6 pt-2 pb-2"> <div class="card"> <div class="card-body"> <h5 class="card-title">`;
 			var cardDataTwo = `</h5> <p class="card-text">`;
@@ -58,6 +53,12 @@ if (comSymbol !== undefined) {
 			}
 
 			document.getElementById("valueDetails").innerHTML = cardData;
+			displayChart(
+				data.SharesOutstanding,
+				data.SharesFloat,
+				data.SharesShort,
+				data.SharesShortPriorMonth
+			);
 		});
 	document.getElementById("default").style.display = "none";
 } else {
@@ -68,51 +69,63 @@ if (comSymbol !== undefined) {
 }
 
 // Chart.js Below
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-	type: "bar",
-	data: {
-		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-		datasets: [
-			{
-				label: "# of Votes",
-				data: [12, 19, 3, 5, 2, 3],
-				backgroundColor: [
-					"rgba(255, 99, 132, 0.2)",
-					"rgba(54, 162, 235, 0.2)",
-					"rgba(255, 206, 86, 0.2)",
-					"rgba(75, 192, 192, 0.2)",
-					"rgba(153, 102, 255, 0.2)",
-					"rgba(255, 159, 64, 0.2)",
-				],
-				borderColor: [
-					"rgba(255, 99, 132, 1)",
-					"rgba(54, 162, 235, 1)",
-					"rgba(255, 206, 86, 1)",
-					"rgba(75, 192, 192, 1)",
-					"rgba(153, 102, 255, 1)",
-					"rgba(255, 159, 64, 1)",
-				],
-				borderWidth: 1,
-			},
-		],
-	},
-	options: {
-		scales: {
-			yAxes: [
+function displayChart(
+	SharesOutstanding,
+	SharesFloat,
+	SharesShort,
+	SharesShortPriorMonth
+) {
+	var ctx = document.getElementById("myChart").getContext("2d");
+	var myChart = new Chart(ctx, {
+		type: "bar",
+		data: {
+			labels: [
+				"SharesOutstanding",
+				"SharesFloat",
+				"SharesShort",
+				"SharesShortPriorMonth",
+			],
+			datasets: [
 				{
-					ticks: {
-						beginAtZero: true,
-					},
+					label: ["Let me figure this out"],
+					data: [
+						SharesOutstanding,
+						SharesFloat,
+						SharesShort,
+						SharesShortPriorMonth,
+					],
+					backgroundColor: [
+						"rgba(255, 99, 132, 0.2)",
+						"rgba(54, 162, 235, 0.2)",
+						"rgba(255, 206, 86, 0.2)",
+						"rgba(75, 192, 192, 0.2)",
+					],
+					borderColor: [
+						"rgba(255, 99, 132, 1)",
+						"rgba(54, 162, 235, 1)",
+						"rgba(255, 206, 86, 1)",
+						"rgba(75, 192, 192, 1)",
+					],
+					borderWidth: 1,
 				},
 			],
 		},
-	},
-});
+		options: {
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							beginAtZero: true,
+						},
+					},
+				],
+			},
+		},
+	});
+}
 
 function redirectCompany() {
 	var companyID = document.getElementById("cmp-id").value;
-	console.log(companyID);
 	window.location.href = "?s=" + companyID;
 }
 
