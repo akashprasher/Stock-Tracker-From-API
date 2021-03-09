f249f3b0be411993d4fcfadc5058886a9a4b30b6
akash prasher <prince.akash.prasher@gmail.com>
2021-03-09 10:01:20 +0530

    fetch() //ing data
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
-// API Link
-// https://www.alphavantage.co/query?function=OVERVIEW&symbol=GME&apikey=TB44BTAI1KG68RMV
+// Fetching Data From API
+var apiUrl =
+	"https://www.alphavantage.co/query?function=OVERVIEW&symbol=GME&apikey=TB44BTAI1KG68RMV";
+
+fetch(apiUrl)
+	.then((data) => data.json())
+	.then((data) => console.log(data));
